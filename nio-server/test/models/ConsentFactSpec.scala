229c31f0c66ce6e6cc5b47e57104b514bb6d9f7f
Vladimir Bodnartchouk <vladimirbodnar@yahoo.fr>
2018-05-31 18:13:43 +0200

    Merge branch 'master' of /Users/vladimirbodnar/workdir/nio-oss with conflicts.

--- nio-server/test/models/ConsentFactSpec.scala
+++ nio-server/test/models/ConsentFactSpec.scala
@@ -8,27 +8,53 @@ class ConsentFactSpec extends PlaySpec with WordSpecLike with MustMatchers {
 
   "ConsentFact" should {
 
-    "serialize/deserialize from XML" in {
-      val consentFact = ConsentFact(
-        _id = "cf",
-        userId = "userId4",
-        doneBy = DoneBy("a1", "admin"),
-        version = 1,
-        groups = Seq(
-          ConsentGroup("a", "a", Seq(Consent("a", "a", false))),
-          ConsentGroup("b", "b", Seq(Consent("b", "b", false)))
-        ),
-        lastUpdate = DateTime.now(DateTimeZone.UTC)
-      )
+    val mdKey1 = "toto"
+    val mdVal1 = "blabla"
 
+    val consentFact = ConsentFact(
+      _id = "cf",
+      userId = "userId4",
+      doneBy = DoneBy("a1", "admin"),
+      version = 1,
+      groups = Seq(
+        ConsentGroup("a", "a", Seq(Consent("a", "a", false))),
+        ConsentGroup("b", "b", Seq(Consent("b", "b", false)))
+      ),
+      lastUpdate = DateTime.now(DateTimeZone.UTC),
+      metaData = Some(Map(mdKey1 -> mdVal1, "tata" -> "val2"))
+    )
+
+    "serialize/deserialize from XML" in {
       val xml = consentFact.asXml
 
       val fromXml = ConsentFact.fromXml(xml)
 
+      println(" --- >  " + xml)
+
       fromXml.isRight mustBe true
-      fromXml.right.get.userId mustBe consentFact.userId
+      val cf = fromXml.right.get
+      cf.userId mustBe consentFact.userId
+      cf.metaData.isDefined mustBe true
+
+      val md = cf.metaData.get
+      md(mdKey1) mustBe mdVal1
     }
 
+    "serialize/deserialize from JSON" in {
+      val json = consentFact.asJson
+
+      println(" --- >  " + json)
+
+      val fromJson = ConsentFact.fromJson(json)
+
+      fromJson.isRight mustBe true
+      val cf = fromJson.right.get
+      cf.userId mustBe consentFact.userId
+      cf.metaData.isDefined mustBe true
+
+      val md = cf.metaData.get
+      md(mdKey1) mustBe mdVal1
+    }
   }
 
 }
