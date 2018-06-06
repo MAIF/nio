26fa02b07c3d360a775ca982259cf347cfdc8ca7
Vladimir Bodnartchouk <vladimirbodnar@yahoo.fr>
2018-06-06 15:11:51 +0200

    remove warning

--- nio-server/app/Starter.scala
+++ nio-server/app/Starter.scala
@@ -2,10 +2,8 @@ import java.util.concurrent.TimeUnit
 
 import akka.actor.ActorSystem
 import akka.stream.ActorMaterializer
-import configuration.Env
 import javax.inject.{Inject, Singleton}
 import db._
-import messaging.KafkaMessageBroker
 import models.Tenant
 
 import play.api.Configuration
@@ -17,22 +15,20 @@ import scala.concurrent.duration.Duration
 import scala.concurrent.{Await, ExecutionContext, Future}
 
 @Singleton
-class Starter @Inject()(config: Configuration,
-                        tenantStore: TenantMongoDataStore,
-                        organisationStore: OrganisationMongoDataStore,
-                        userStore: UserMongoDataStore,
-                        consentFactStore: ConsentFactMongoDataStore,
-                        accountMongoDataStore: AccountMongoDataStore,
-                        destroyTaskMongoDataStore: DeletionTaskMongoDataStore,
-                        extractionTaskMongoDataStore: ExtractionTaskMongoDataStore,
-                        kafkaMessageBroker: KafkaMessageBroker,
-                        defaultLoader: DefaultLoader,
-                        secureEvent: SecureEvent,
-                        s3: S3,
-                        env: Env,
-                        applicationLifecycle: ApplicationLifecycle)(
-    implicit ec: ExecutionContext,
-    system: ActorSystem) {
+class Starter @Inject()(
+    config: Configuration,
+    tenantStore: TenantMongoDataStore,
+    organisationStore: OrganisationMongoDataStore,
+    userStore: UserMongoDataStore,
+    consentFactStore: ConsentFactMongoDataStore,
+    accountMongoDataStore: AccountMongoDataStore,
+    destroyTaskMongoDataStore: DeletionTaskMongoDataStore,
+    extractionTaskMongoDataStore: ExtractionTaskMongoDataStore,
+    defaultLoader: DefaultLoader,
+    secureEvent: SecureEvent,
+    s3: S3,
+    applicationLifecycle: ApplicationLifecycle)(implicit ec: ExecutionContext,
+                                                system: ActorSystem) {
 
   val dbFlush = config.get[Boolean]("db.flush")
   val tenants = config.get[Seq[String]]("db.tenants")
@@ -47,18 +43,19 @@ class Starter @Inject()(config: Configuration,
         _ <- Future.sequence(tenants.map { tenant =>
           tenantStore.insert(Tenant(tenant, "Default tenant from config file"))
         })
-        _ <- Future.sequence(tenants.map { tenant =>
-          for {
-            _ <- organisationStore.init(tenant)
-            _ <- userStore.init(tenant)
-            _ <- consentFactStore.init(tenant)
-            _ <- accountMongoDataStore.init(tenant)
-            _ <- destroyTaskMongoDataStore.init(tenant)
-            _ <- extractionTaskMongoDataStore.init(tenant)
-            _ <- defaultLoader.load(tenant)
-          } yield {
-            ()
-          }
+        _ <- Future.sequence(tenants.map {
+          tenant =>
+            for {
+              _ <- organisationStore.init(tenant)
+              _ <- userStore.init(tenant)
+              _ <- consentFactStore.init(tenant)
+              _ <- accountMongoDataStore.init(tenant)
+              _ <- destroyTaskMongoDataStore.init(tenant)
+              _ <- extractionTaskMongoDataStore.init(tenant)
+              _ <- defaultLoader.load(tenant)
+            } yield {
+              ()
+            }
         })
         _ = s3.startExpiredFilesCleaner
       } yield (),
